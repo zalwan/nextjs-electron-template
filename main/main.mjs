f39c478fe495edd601d2da8f1edec71a3060abd2
rizal suryawan <rizalsuryawan86@gmail.com>
2024-05-29 10:23:45 +0700

    feat: add electronjs

---
+++ main/main.mjs
@@ -0,0 +1,49 @@
+// main.mjs
+import { app, BrowserWindow } from "electron";
+import serve from "electron-serve";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const appServe = app.isPackaged
+  ? serve({
+      directory: path.join(__dirname, "out"),
+    })
+  : null;
+
+const createWindows = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      // inspect element disable on production
+      devTools: !app.isPackaged,
+    },
+  });
+
+  if (app.isPackaged) {
+    appServe(mainWindow).then(() => {
+      mainWindow.loadURL("app://-");
+    });
+  } else {
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.on("did-fail-load", (e, code, desc) => {
+      mainWindow.webContents.reloadIgnoringCache();
+    });
+  }
+};
+
+app.on("ready", () => {
+  createWindows();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
---
+++ main/preload.js
@@ -0,0 +1,12 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+console.log("Preload script loaded");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  on: (channel, callback) => {
+    ipcRenderer.on(channel, callback);
+  },
+  send: (channel, args) => {
+    ipcRenderer.send(channel, args);
+  },
+});
